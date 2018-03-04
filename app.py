994e62a8d05f96628759fe121ddbba9233824daa
xirapo <fgarciadiaz29@gmail.com>
2018-03-04 16:12:54 -0600

    require login for admin

--- app.py
+++ app.py
@@ -1,9 +1,27 @@
 # imports
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, \
+session, flash
+
+from functools import wraps
 
 app = Flask(__name__)
 
+# secret key for session
+app.secret_key = "12345678"
+
 # Routes
+
+# login require
+
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You need to login first.')
+            return redirect(url_for('login'))
+    return wrap
 
 ## home
 @app.route('/')
@@ -16,13 +34,22 @@ def login():
     if request.method == 'POST':
         if request.form['username'] != 'admin' or request.form['password'] \
          != 'admin':
-            error = "Invalid credentials. Please try again."
+            error = "Wrong username or password."
         else:
+            session['logged_in'] = True
             return redirect(url_for('admin'))
     return render_template("login.html", err=error)
 
+# logout
+@app.route('/logout')
+@login_required
+def logout():
+    session.pop('logged_in', None)
+    return redirect(url_for('home'))
+
 ## admin
 @app.route('/admin')
+@login_required
 def admin():
     return render_template("admin.html")
 
